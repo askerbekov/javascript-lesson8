3191d1d6e3e5cb79cc0d5f2083fe97e3f52606c2
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-19 20:47:09 +0600

    added homework reduce and function for

---
+++ script.js
@@ -0,0 +1,41 @@
+// function arraySumm (arr){
+//     let result = 0
+//     for (let i = 0 ; i < arr.length ; i++){
+//         result += arr[i]
+//     }
+//  return result
+// }
+//
+// console.log(arraySumm( [100, 20, 3, 4, 5]))
+
+
+// function arraySummResult ( arr){
+//  return arr.reduce((acc ,el , idx) => {
+//     return acc + el
+// } ,0)
+// }
+// console.log(arraySummResult( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+git init
+
+// function arrayResult (arr) {
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] === 4) {
+//             return 'yes'
+//         }
+//     }
+//     return 'no'
+//
+// }
+// console.log(arrayResult(  [1, 2, 5, 9, 4,  13, 10]))
+
+
+// function arrResult (arr){
+//     let ersult = 0
+//     for (let i = 0 ; i < arr.length; i++ ) {
+//         ersult = ersult +  arr[i]
+//
+//     }
+//     return ersult
+//
+// }
+// console.log(arrResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
